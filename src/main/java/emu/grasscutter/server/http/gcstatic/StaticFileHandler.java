3927877cb62b1febfeb51aa47fe9182db78acb46
lakwsh <lakwsh@lakwsh.net>
2022-05-02 19:55:06 +0800

    Update StaticFileHandler.java

--- src/main/java/emu/grasscutter/server/http/gcstatic/StaticFileHandler.java
+++ src/main/java/emu/grasscutter/server/http/gcstatic/StaticFileHandler.java
@@ -11,21 +11,22 @@ import express.http.Response;
 public final class StaticFileHandler implements HttpContextHandler {
 	String static_folder;
 	public StaticFileHandler() {
-        static_folder = Grasscutter.getConfig().RESOURCE_FOLDER + "/gcstatic";
+		static_folder = Grasscutter.getConfig().RESOURCE_FOLDER + "/gcstatic";
 	}
 
 	@Override
 	public void handle(Request req, Response res) throws IOException {
-        // Grasscutter.getLogger().info( req.path());
+		// Grasscutter.getLogger().info( req.path());
 
-        String reqFilename = req.path().replace("/gcstatic", ""); // remove the leading path
-        reqFilename = reqFilename.replace("/../", "/./"); // security guard to prevent arbitrary read
-        File resFile = new File(static_folder + reqFilename);
-        if (resFile.exists()) {
-            res.sendFile(resFile.toPath());
-        } else {
-            res.status(404);
-            res.send("404");
-        }
-    }
+		String path = req.path(); // remove the leading path
+		if (path.indexOf("./") == -1) {
+			File resFile = new File(static_folder + path.replace("/gcstatic", ""));
+			if (resFile.exists()) {
+				res.sendFile(resFile.toPath());
+				return;
+			}
+		}
+		res.status(404);
+		res.send("404");
+	}
 }
